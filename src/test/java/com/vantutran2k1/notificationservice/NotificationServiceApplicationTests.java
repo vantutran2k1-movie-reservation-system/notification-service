050d37bfedea9e0c1b0c96ca76535f5e902905cd
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-12-28 08:00:18 +0700

    Initial commit

---
+++ src/test/java/com/vantutran2k1/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vantutran2k1.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
