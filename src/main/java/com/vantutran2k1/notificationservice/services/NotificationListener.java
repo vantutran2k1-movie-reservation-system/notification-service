b9bbf882ecc71faced84ceb6d03a593d98843fe9
vantutran2k1 <vantu.tran2k1@gmail.com>
2025-01-24 21:47:19 +0700

    Update email format to include verification token

--- src/main/java/com/vantutran2k1/notificationservice/services/NotificationListener.java
+++ src/main/java/com/vantutran2k1/notificationservice/services/NotificationListener.java
@@ -3,6 +3,8 @@ package com.vantutran2k1.notificationservice.services;
 import com.vantutran2k1.notificationservice.models.UserRegistrationEvent;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +14,18 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class NotificationListener {
+    private static final Logger log = LoggerFactory.getLogger(NotificationListener.class);
     private final EmailService emailService;
 
     @KafkaListener(
-            topics = "user-registrations",
+            topics = "users.user_registrations",
             containerFactory = "userRegistrationListenerContainerFactory"
     )
     public void listen(UserRegistrationEvent event) {
         Map<String, Object> templateModel = new HashMap<>();
         templateModel.put("userName", event.getFirstName() + " " + event.getLastName());
-        templateModel.put("verificationLink", "https://example.com?verificationCode=" + event.getVerificationToken());
+        templateModel.put("verificationLink", "http://localhost:5173/verify-user");
+        templateModel.put("verificationToken", event.getVerificationToken());
 
         try {
             emailService.sendEmail(event.getEmail(), "Welcome to MovieHub", templateModel);
